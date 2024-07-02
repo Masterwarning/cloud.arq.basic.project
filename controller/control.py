8620ca74f8569e818928a7f06f3751c9c6eb3606
m4sterwarning <m4sterwarning@gmail.com>
2024-07-01 23:05:59 -0500

    index(complete), register(in process RDS)

--- controller/control.py
+++ controller/control.py
@@ -2,5 +2,26 @@ from flask import render_template, request
 from database.db import *
 from controller.admin_s3 import *
 
-def ctrl_home():
+def ctrl_index():
     return render_template("index.html")
+
+def ctrl_register_page():
+    print("control")
+    return render_template("register.html")
+
+def ctrl_consult_page():
+    return render_template("consult.html")
+
+def ctrl_register_user():
+    rf = request.form
+    id = rf['id']
+    name = rf['name']
+    lastname = rf['lastname']
+    email = rf['email']
+    birthday = rf['birthday']
+    gender = rf['gender']
+    address = rf['address']
+    user_registration = add_user(id, name, lastname, email, birthday, gender, address)
+    test = str(user_registration)
+    print(test)
+    return test
